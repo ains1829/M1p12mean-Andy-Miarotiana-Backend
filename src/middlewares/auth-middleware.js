46da1c1f3d080a33649308c940281ff0786f9b23
ains1829 <andyrakotonavalona0@gmail.com>
2025-03-24 16:17:42 +0300

    connecte mongodb

--- src/middlewares/auth-middleware.js
+++ src/middlewares/auth-middleware.js
@@ -1,6 +1,20 @@
 const jwt = require("jsonwebtoken");
 const SECRET_KEY = process.env.SECRET_KEY || "ma_clé_secrète";
 
+function middleware_global(req, res, next) {
+  const token = req.headers["authorization"];
+  if (!token) {
+    return res.status(403).json({ message: "Token requis" });
+  }
+  jwt.verify(token.split(" ")[1], SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Token invalide" });
+    }
+    req.user = decoded;
+    next();
+  });
+}
+
 function middleware_auth_client(req, res, next) {
   const token = req.headers["authorization"];
   if (!token) {
@@ -18,4 +32,4 @@ function middleware_auth_client(req, res, next) {
     }
   });
 }
-module.exports = { middleware_auth_client };
+module.exports = { middleware_auth_client, middleware_global };
