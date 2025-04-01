f2b969e74ad54562abc55f21593e5062d50be5c7
Miarotiana Stephan <miarotianastephan01@gmail.com>
2025-04-02 00:33:07 +0300

    Update parts-data.js
    modification des pieces pour les marques

--- src/models/part/data/parts-data.js
+++ src/models/part/data/parts-data.js
@@ -1,8 +1,8 @@
 const parts = [
   {
-    brandId: "67d07d347f397f04e37e7163", // Tesla
+    brandId: "67ec5a0912249517291bc844", // Tesla
     brand: "Tesla",
-    modelId: "67d07d347f397f04e37e7164", // Model S
+    modelId: "67ec5a0912249517291bc845", // Model S
     model: "Model S",
     partName: "Disque de frein avant",
     description: "Disque de frein avant compatible Tesla Model S",
@@ -13,94 +13,75 @@ const parts = [
     ],
   },
   {
-    brandId: "67d07d347f397f04e37e7163", // Tesla
-    brand: "Tesla",
-    modelId: "67d07d347f397f04e37e7164", // Model S
-    model: "Model S",
-    partName: "Plaquettes de frein avant",
-    description: "Plaquettes de frein avant pour Tesla Model S",
-    price: 75.0,
-    images: ["https://exemple.com/images/plaquettes-frein-avant.jpg"],
+    brandId: "67ec5a0912249517291bc849", // BMW
+    brand: "BMW",
+    modelId: "67ec5a0912249517291bc84a", // i3
+    model: "i3",
+    partName: "Batterie haute tension",
+    description: "Batterie haute tension pour BMW i3",
+    price: 5000.0,
+    images: ["https://exemple.com/images/batterie-i3.jpg"],
   },
   {
-    brandId: "67d07d347f397f04e37e7163", // Tesla
-    brand: "Tesla",
-    modelId: "67d07d347f397f04e37e7164", // Model S
-    model: "Model S",
-    partName: "Amortisseurs avant",
-    description: "Amortisseurs avant pour Tesla Model S",
-    price: 250.0,
-    images: ["https://exemple.com/images/amortisseurs-avant.jpg"],
+    brandId: "67ec5a0912249517291bc84e", // Hyundai
+    brand: "Hyundai",
+    modelId: "67ec5a0912249517291bc84f", // Kona Electric
+    model: "Kona Electric",
+    partName: "Chargeur embarqué",
+    description: "Chargeur embarqué pour Hyundai Kona Electric",
+    price: 800.0,
+    images: ["https://exemple.com/images/chargeur-kona.jpg"],
   },
   {
-    brandId: "67d07d347f397f04e37e7163", // Tesla
-    brand: "Tesla",
-    modelId: "67d07d347f397f04e37e7164", // Model S
-    model: "Model S",
-    partName: "Pneus été",
-    description: "Pneus été pour Tesla Model S",
-    price: 600.0,
-    images: ["https://exemple.com/images/pneus-ete.jpg"],
+    brandId: "67ec5a0912249517291bc852", // Audi
+    brand: "Audi",
+    modelId: "67ec5a0912249517291bc853", // e-tron
+    model: "e-tron",
+    partName: "Système de freinage régénératif",
+    description: "Système de freinage régénératif pour Audi e-tron",
+    price: 1200.0,
+    images: ["https://exemple.com/images/freinage-etron.jpg"],
   },
   {
-    brandId: "67d07d347f397f04e37e7163", // Tesla
-    brand: "Tesla",
-    modelId: "67d07d347f397f04e37e7164", // Model S
-    model: "Model S",
-    partName: "Batterie 12V",
-    description: "Batterie 12V pour Tesla Model S",
-    price: 150.0,
-    images: ["https://exemple.com/images/batterie-12v.jpg"],
+    brandId: "67ec5a0912249517291bc855", // Porsche
+    brand: "Porsche",
+    modelId: "67ec5a0912249517291bc856", // Taycan
+    model: "Taycan",
+    partName: "Moteur électrique",
+    description: "Moteur électrique pour Porsche Taycan",
+    price: 10000.0,
+    images: ["https://exemple.com/images/moteur-taycan.jpg"],
   },
   {
-    brandId: "67d07d347f397f04e37e7163", // Tesla
-    brand: "Tesla",
-    modelId: "67d07d347f397f04e37e7164", // Model S
-    model: "Model S",
-    partName: "Essuie-glaces avant",
-    description: "Essuie-glaces avant pour Tesla Model S",
-    price: 30.0,
-    images: ["https://exemple.com/images/essuie-glaces.jpg"],
+    brandId: "67ec5a0912249517291bc857", // Volkswagen
+    brand: "Volkswagen",
+    modelId: "67ec5a0912249517291bc858", // ID.3
+    model: "ID.3",
+    partName: "Onduleur",
+    description: "Onduleur pour Volkswagen ID.3",
+    price: 1500.0,
+    images: ["https://exemple.com/images/onduleur-id3.jpg"],
   },
   {
-    brandId: "67d07d347f397f04e37e7163", // Tesla
-    brand: "Tesla",
-    modelId: "67d07d347f397f04e37e7164", // Model S
-    model: "Model S",
-    partName: "Feux arrière",
-    description: "Feux arrière pour Tesla Model S",
-    price: 220.0,
-    images: ["https://exemple.com/images/feux-arriere.jpg"],
+    brandId: "67ec5a0912249517291bc85b", // Mercedes-Benz
+    brand: "Mercedes-Benz",
+    modelId: "67ec5a0912249517291bc85c", // EQC
+    model: "EQC",
+    partName: "Pompe à chaleur",
+    description: "Pompe à chaleur pour Mercedes-Benz EQC",
+    price: 1800.0,
+    images: ["https://exemple.com/images/pompe-chaleur-eqc.jpg"],
   },
   {
-    brandId: "67d07d347f397f04e37e7163", // Tesla
-    brand: "Tesla",
-    modelId: "67d07d347f397f04e37e7164", // Model S
-    model: "Model S",
-    partName: "Câble de recharge",
-    description: "Câble de recharge pour Tesla Model S",
-    price: 180.0,
-    images: ["https://exemple.com/images/cable-recharge.jpg"],
-  },
-  {
-    brandId: "67d07d347f397f04e37e7163", // Tesla
-    brand: "Tesla",
-    modelId: "67d07d347f397f04e37e7164", // Model S
-    model: "Model S",
-    partName: "Clé de voiture",
-    description: "Clé de voiture Tesla Model S",
-    price: 50.0,
-    images: ["https://exemple.com/images/cle-voiture.jpg"],
-  },
-  {
-    brandId: "67d07d347f397f04e37e7163", // Tesla
-    brand: "Tesla",
-    modelId: "67d07d347f397f04e37e7164", // Model S
-    model: "Model S",
-    partName: "Filtre d'habitacle",
-    description: "Filtre d'habitacle pour Tesla Model S",
-    price: 40.0,
-    images: ["https://exemple.com/images/filtre-habitacle.jpg"],
+    brandId: "67ec5a0912249517291bc85e", // Kia
+    brand: "Kia",
+    modelId: "67ec5a0912249517291bc85f", // EV6
+    model: "EV6",
+    partName: "Afficheur tête haute",
+    description: "Afficheur tête haute pour Kia EV6",
+    price: 1000.0,
+    images: ["https://exemple.com/images/hud-ev6.jpg"],
   },
 ];
+
 module.exports = parts;
